f3d2f03898f3b47f963293487b8d5784203aef7e
Nattapol Kamsaeng <nattapol@nattapolk.(none)>
2013-08-23 01:22:44 -0500

    initial commit

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var fs = require('fs');
+    var content = fs.readFileSync('index.html').toString();
+    response.send(content);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
